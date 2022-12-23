dee61ed0e320453a9f98a1e67a820a8c52c26a7d
arthur <ERHUE@EX.UA>
2022-12-23 13:37:38 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Arthur Sh on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
