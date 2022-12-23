57a45e8a040b90a1593fcbfe1b9a091530d217a7
arthur <ERHUE@EX.UA>
2022-12-23 14:24:02 +0200

    - add new static example for previewing struct of location - add new star circle custom map annotation item - add new Comparable method == to compare only with id - add new computed coordinates property

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Location: Identifiable, Codable, Equatable {
     let id: UUID
@@ -13,4 +14,14 @@ struct Location: Identifiable, Codable, Equatable {
     var description: String
     let latitude: Double
     let longitude: Double
+    
+    var coordinates: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Jurassic park", description: "That's where all the ancient creatures are living", latitude: 50, longitude: -13)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
